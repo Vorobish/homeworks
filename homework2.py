3b052c41582cecfd1db6997471226b167ea8464e
Настя Х <nastysha7069@yandex.ru>
2024-05-04 16:05:49 +0300

    Task_240504_230923_dynamic_typing

---
+++ homework2.py
@@ -0,0 +1,19 @@
+name = 'Nastya'
+print(name)
+age = 31
+print(age)
+age = age + 1
+print(age)
+is_student = True
+print(is_student)
+
+#если результат нужен как в задании, то
+
+name = 'Nastya'
+print('Name:', name)
+age = 31
+print('Age:', age)
+age = age + 1
+print('New Age:', age)
+is_student = True
+print('Is Student:', is_student)
