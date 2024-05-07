b1c8f71c6a833dd279bed826982d3ff5a2ce9a2c
Настя Х <nastysha7069@yandex.ru>
2024-05-07 22:52:02 +0300

    Task_240506_230925_list_dict

---
+++ homework5.py
@@ -0,0 +1,17 @@
+#2
+my_list = ['apple', 'peach', 'banana', 'coconut', 'orange', 'pear']
+print('List:', my_list)
+print('First element:', my_list[0])
+print('Last element:', my_list[-1])
+print('3-5 element:', my_list[2:5]) #если 5-ый должен входить
+my_list[2] = 'apricot'
+print('Modified list:', my_list)
+#3
+print() #отступ
+my_dict = {'Денис': 'Denis', 'Аня': 'Ann', 'Настя': 'Nastya', 'Саша': 'Sasha'}
+print('Dictionary:', my_dict)
+print('Translation:', my_dict['Настя'])
+my_dict['Мила'] = 'Mila'
+print('Modified dictionary:', my_dict)
+
+
