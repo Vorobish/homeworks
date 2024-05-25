d59b7506dcc92bdaa05629e510befff08768e58f
Настя Х <nastysha7069@yandex.ru>
2024-05-25 19:26:33 +0300

    Модуль 4. ДЗ 25.05.24 Модули и пакеты

---
+++ main.py
@@ -0,0 +1,16 @@
+from fake_math import divide as d_fk
+from true_math import divide as d_tr
+
+print(d_fk(5, 0))
+print(d_tr(5, 0))
+
+result1 = d_fk(69, 3)
+result2 = d_fk(3, 0)
+result3 = d_tr(49, 7)
+result4 = d_tr(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+
