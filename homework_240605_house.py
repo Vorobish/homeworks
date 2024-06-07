87838b65aa568b8a292af9226ea6052a56fb6031
Настя Х <nastysha7069@yandex.ru>
2024-06-07 22:39:54 +0300

    Модуль 5. "Атрибуты и методы объекта" про ЖК

---
+++ homework_240605_house.py
@@ -0,0 +1,20 @@
+class House:
+
+    def __init__(self, name, num):
+        self.name = name
+        self.number_of_floors = num
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            i = 1
+            while i <= new_floor:
+                print(i)
+                i += 1
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
