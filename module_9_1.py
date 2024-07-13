69c51949aae245037af1322d16a958e15f307971
Настя Х <nastysha7069@yandex.ru>
2024-07-13 18:24:11 +0300

    Задача "Вызов разом":

---
+++ module_9_1.py
@@ -0,0 +1,11 @@
+
+def apply_all_func(int_list, *function):
+    list_ = list(function)
+    res = {}
+    for f in list_:
+        res.update({f.__name__: f(int_list)})
+    return res
+
+
+print(apply_all_func([6, 20, 15, 9], max, min), end='')
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
